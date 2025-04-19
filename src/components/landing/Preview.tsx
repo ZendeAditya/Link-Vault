7634b4e68a75f49291499a4d5b88ac0aa5e8358e
ZendeAditya <adityazende6710@gmail.com>
2025-04-20 04:27:38 +0530

    chore(landing): added formatted components

---
+++ src/components/landing/Preview.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Preview = () => {
+    return (
+        <section className="w-screen h-screen flex items-center justify-center bg-gradient-to-b from-[#1e1e2f] to-[#1e1e2f] text-white">
+            <h1>Welcome to the Preview</h1>
+        </section>
+    )
+}
+
+export default Preview
