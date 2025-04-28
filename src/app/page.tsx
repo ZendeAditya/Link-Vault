b3406e99902e97d8c779e249d33d85be64020f23
ZendeAditya <adityazende6710@gmail.com>
2025-04-28 15:36:07 +0530

    chore: signup and signin component design completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import Hero from '@/components/landing/Hero'
-import Navbar from '@/components/landing/Navbar'
 import Features from '../components/landing/Features'
 import Preview from '@/components/landing/Preview'
 import Footer from '@/components/landing/Footer'
@@ -7,7 +6,6 @@ import Footer from '@/components/landing/Footer'
 export default function Home() {
     return (
         <div className="relative">
-            <Navbar />
             <Hero />
             <Features />
             <Preview />
